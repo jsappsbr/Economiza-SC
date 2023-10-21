8ee6eddc89490920e231bbbef4d7d42468094717
João Salomão <joaosalomao.mcp@gmail.com>
2023-10-21 17:20:48 -0300

    Getting products from different stores

--- backend/resources/js/scraper/supermercadoKoch.js
+++ backend/resources/js/scraper/supermercadoKoch.js
@@ -1,45 +1,67 @@
 import puppeteer from 'puppeteer';
 import fs from 'node:fs';
 
+// Got from the stores modal select that shows up when a
+// new browser is opened and the store is not defined yet
+const STORES_SELECT_VALUES = {
+    TIJUCAS: 'website_lj9',
+    ITAPEMA: 'website_lj9',
+    PORTO_BELO: 'website_lj9',
+    CAMBORIU: 'website_lj5',
+    ITAJAI: 'website_lj5',
+    BALNEARIO_CAMBORIU: 'website_lj5',
+    BARRA_VELHA: 'website_lj23',
+    PENHA: 'website_lj23',
+    PICARRAS: 'website_lj23',
+    FLORIANOPOLIS: 'website_lj47',
+    NAVEGANTES: 'website_lj44',
+};
+
+const ALL_PAGES = [
+    'https://www.superkoch.com.br/carnes',
+    'https://www.superkoch.com.br/mercearia',
+    'https://www.superkoch.com.br/frios-laticinios',
+    'https://www.superkoch.com.br/horta-pomar-granja-floric',
+    'https://www.superkoch.com.br/bebidas',
+    'https://www.superkoch.com.br/limpeza',
+    'https://www.superkoch.com.br/higiene',
+    'https://www.superkoch.com.br/outros'
+];
+
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: 'new',
         devtools: false,
         defaultViewport: null,
         args: ['--start-maximized']
     });
 
-    const pageLinks = [
-        'https://www.superkoch.com.br/mercearia',
-        'https://www.superkoch.com.br/frios-laticinios',
-        'https://www.superkoch.com.br/carnes',
-        'https://www.superkoch.com.br/horta-pomar-granja-floric',
-        'https://www.superkoch.com.br/bebidas',
-        'https://www.superkoch.com.br/limpeza',
-        'https://www.superkoch.com.br/higiene',
-        'https://www.superkoch.com.br/outros'
-    ]
+    const allProducts = {}
 
-    const allProducts = []
+    for (const initialPageLink of ALL_PAGES) {
+        allProducts[initialPageLink] = {}
 
-    for (const initialPageLink of pageLinks) {
-        const products = await scrapeProducts(browser, initialPageLink)
-        allProducts.push(...products)
+        for (const storeValue of [...new Set(Object.values(STORES_SELECT_VALUES))]) {
+            allProducts[initialPageLink][storeValue] = await scrapeProducts(browser, initialPageLink, storeValue)
+        }
     }
 
-    fs.writeFileSync('storage/app/scraper/products.json', JSON.stringify(allProducts, null, 2))
-
     await browser.close();
+
+    fs.writeFileSync('storage/app/scraper/products.json', JSON.stringify(allProducts, null, 2))
 })();
 
-async function scrapeProducts(browser, pageLink) {
+async function scrapeProducts(browser, pageLink, storeValue) {
     const allProducts = []
     let currentPageLink = pageLink
 
     while (true) {
-        console.info(`Scraping ${currentPageLink}`)
+        console.info(`Scraping ${currentPageLink} for store ${storeValue}`)
 
         const page = await browser.newPage();
+
+        // This is the used by the website to know which store to use
+        await page.setCookie({name: 'customer_website', value: storeValue, domain: 'www.superkoch.com.br', path: '/'})
 
         await page.goto(currentPageLink);
 
