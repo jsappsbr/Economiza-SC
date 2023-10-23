de9ab52b6d035ce8a6e4524ab7b83d2fc15beb58
Gustavo Ramos <gustavoloureiroramos@gmail.com>
2023-10-23 10:17:45 +0200

    logo and alignment added to the login page

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:anotei/stores/auth_store.dart';
+import 'package:anotei/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class LoginPage extends StatelessWidget {
@@ -9,7 +11,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       // appBar: AppBar(title: const Text('Login')),
-      body: LoginForm(),
+      body: Center(child: LoginForm()),
     );
   }
 }
@@ -29,55 +31,64 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(hintText: 'E-mail'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Campo obrigatório';
-                }
+    return Observer(builder: (_) {
+      if (_authStore.isFetchingCurrentUser) {
+        return const CircularProgressIndicator();
+      }
 
-                return null;
-              },
-            ),
-            TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(hintText: 'Senha'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Campo obrigatório';
-                  }
+      return ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 400),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const LogoWidget(customAsset: 'assets/images/logo_anotei_round.png'),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
 
-                  return null;
-                }),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: ElevatedButton(
-                onPressed: () => _authStore.isAuthenticating ? null : _handleSubmit(context),
-                child: _authStore.isAuthenticating
-                    ? const SizedBox(
-                  width: 24,
-                  height: 24,
-                  child: CircularProgressIndicator(
-                    color: Colors.white,
+                    return null;
+                  },
+                ),
+                TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(hintText: 'Senha'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+
+                      return null;
+                    }),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: ElevatedButton(
+                    onPressed: () => _authStore.isAuthenticating ? null : _handleSubmit(context),
+                    child: _authStore.isAuthenticating
+                        ? const SizedBox(
+                            width: 24,
+                            height: 24,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                            ),
+                          )
+                        : const Text('Entrar'),
                   ),
-                )
-                    : const Text('Entrar'),
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   _handleSubmit(BuildContext context) {
