7c18516728d05201ddabded9efb898fe6adf07db
João Salomão <joaosalomao.mcp@gmail.com>
2023-10-24 13:06:30 -0300

    Starting final scraper for Fort Atacadista

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Store;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,43 +14,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@test.com',
             'password' => bcrypt('password'),
         ]);
 
-        \App\Models\Store::factory()->create([
+        Store::factory()->create([
             'name' => 'Supermercado Koch - Itajaí e Região',
             'website' => 'https://www.superkoch.com.br',
             'code' => 'website_lj5',
         ]);
 
-        \App\Models\Store::factory()->create([
+        Store::factory()->create([
             'name' => 'Supermercado Koch - Itapema e Região',
             'website' => 'https://www.superkoch.com.br',
             'code' => 'website_lj9',
         ]);
 
-        \App\Models\Store::factory()->create([
+        Store::factory()->create([
             'name' => 'Supermercado Koch - Penha e Região',
             'website' => 'https://www.superkoch.com.br',
             'code' => 'website_lj23',
         ]);
 
-        \App\Models\Store::factory()->create([
+        Store::factory()->create([
             'name' => 'Supermercado Koch - Florianópolis Ilha',
             'website' => 'https://www.superkoch.com.br',
             'code' => 'website_lj47',
         ]);
 
-        \App\Models\Store::factory()->create([
+        Store::factory()->create([
             'name' => 'Supermercado Koch - Florianópolis Ilha',
             'website' => 'https://www.superkoch.com.br',
             'code' => 'website_lj47',
         ]);
 
-        \App\Models\Store::factory()->create([
+        Store::factory()->create([
             'name' => 'Supermercado Koch - Navegantes',
             'website' => 'https://www.superkoch.com.br',
             'code' => 'website_lj44',
