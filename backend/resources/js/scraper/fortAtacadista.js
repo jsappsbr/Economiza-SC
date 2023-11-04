6fe386e03f2620af96cea4eacc903ee04d3c1be2
João Salomão <joaosalomao.mcp@gmail.com>
2023-11-04 00:28:18 -0300

    Getting products from app stores

--- backend/resources/js/scraper/fortAtacadista.js
+++ backend/resources/js/scraper/fortAtacadista.js
@@ -1,12 +1,47 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer'
 import fs from "node:fs";
 
+const MARKETS = [
+    {
+        id: 'Lj115',
+        name: 'Balneário Camboriú - Estados',
+        zip: '88339-035',
+    },
+    // {
+    //     id: 'Lj310',
+    //     name: 'Itajaí - Centro',
+    //     zip: '88301-360',
+    // },
+    // {
+    //     id: 'Lj195',
+    //     name: 'Blumenau - Itoupavazinha',
+    //     zip: '89066-001',
+    // },
+    // {
+    //     id: 'Lj810',
+    //     name: 'Florianópolis - Santo Antônio de Lisboa',
+    //     zip: '88050-001',
+    // },
+    // {
+    //     id: 'Lj810',
+    //     name: 'São José - Kobrasol',
+    //     zip: '88102-340',
+    // },
+    // {
+    //     id: 'LJ145',
+    //     name: 'Florianópolis - Campeche',
+    //     zip: '88065-000',
+    // },
+]
+
+
 const run = async () => {
+
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         devtools: false,
         defaultViewport: null,
-        args: ['--start-maximized']
+        args: ['--start-maximized'],
     });
 
     const pageLinks = [
@@ -27,25 +62,71 @@ const run = async () => {
 
     const allProducts = []
 
-    for (const initialPageLink of pageLinks) {
-        const products = await scrapeProducts(browser, initialPageLink)
-        allProducts.push(...products)
+    const page = await browser.newPage();
+    page.setDefaultTimeout(60_000)
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+        if (request.url('https://af-origin.vtex.com/api/activity-flow/page-views')) {
+            // request.respond({status: 200})
+        } else if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
+            request.abort();
+        } else {
+            request.continue();
+        }
+    });
+
+    for (const market of MARKETS) {
+        await setMarket(page, market)
+
+        for (const initialPageLink of pageLinks) {
+            const products = await scrapeProducts(page, initialPageLink)
+            allProducts.push(...products)
+        }
     }
+
+    await page.close()
 
     fs.writeFileSync('storage/app/scraper/fort_atacadista_products.json', JSON.stringify(allProducts, null, 2))
 
     await browser.close();
 }
 
-async function scrapeProducts(browser, pageLink) {
+async function setMarket(page, market) {
+    await page.goto('https://www.deliveryfort.com.br/pet-shop');
+    await page.waitForSelector('.modalCep-content--insertPostalCode-postalCodeContainer')
+    await page.evaluate((market) => {
+        const input = document.querySelector('.modalCep-content--insertPostalCode-postalCodeContainer input')
+        input.value = market.zip
+
+        const button = document.querySelector('#cepInsertedAction')
+        button.click()
+    }, market)
+
+    await page.waitForNetworkIdle()
+
+    const marketInput = `input[data-id="${market.id}"]`
+
+    await page.waitForSelector(marketInput)
+    await page.evaluate((marketInput) => {
+        const input = document.querySelector(marketInput)
+        input.click()
+
+        const button = document.querySelector('#deliveryTypeSelected')
+        button.click()
+    }, marketInput)
+
+    await page.waitForNetworkIdle()
+}
+
+async function scrapeProducts(page, pageLink) {
     console.info(`Scraping ${pageLink}`)
 
     const allProducts = []
-
-    const page = await browser.newPage();
     await page.goto(pageLink);
 
     while (true) {
+        await page.waitForNetworkIdle()
+
         const products = await page.evaluate(async () => {
             const products = []
             const listContainer = document.querySelector('div.main-shelf.n32colunas').children[0]
@@ -78,23 +159,20 @@ async function scrapeProducts(browser, pageLink) {
             nextLink.click()
         })
 
-
-        await page.waitForResponse(response => {
-            return response.url().startsWith('https://www.deliveryfort.com.br/buscapagina')
-        });
+        await Promise.all([
+            page.waitForNetworkIdle(),
+            page.waitForSelector('div.main-shelf.n32colunas')
+        ])
 
         const hasMoreProducts = await page.evaluate(() => {
-            return !document.querySelector('div.main-shelf.n32colunas')
+            return document.querySelector('div.main-shelf.n32colunas').children[0].childElementCount !== 0
         })
 
         if (!hasMoreProducts) {
+            console.log('No more products found')
             break
         }
-
     }
-
-    await page.close()
-
 
     return allProducts
 }
