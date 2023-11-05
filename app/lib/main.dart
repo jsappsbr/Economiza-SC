696415f83b20f899c5fbe1ad76b7d18756edb883
João Salomão <joaosalomao.mcp@gmail.com>
2023-11-05 18:23:55 -0300

    Create basic integration test

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:anotei/modules/app_module.dart';
 import 'app_widget.dart';
 
+final app = ModularApp(module: AppModule(), child: const AppWidget());
+
 void main() {
-  return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+  return runApp(app);
 }
--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:anotei/stores/auth_store.dart';
 import 'package:anotei/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class LoginPage extends StatelessWidget {
@@ -29,42 +30,49 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-      return ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 400),
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const LogoWidget(assetPath: 'assets/images/logo_anotei_round.png'),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(hintText: 'E-mail'),
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 400),
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const LogoWidget(
+                  assetPath: 'assets/images/logo_anotei_round.png'),
+              TextFormField(
+                key: const Key('emailField'),
+                controller: _emailController,
+                decoration: const InputDecoration(hintText: 'E-mail'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+
+                  return null;
+                },
+              ),
+              TextFormField(
+                  key: const Key('passwordField'),
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(hintText: 'Senha'),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Campo obrigatório';
                     }
 
                     return null;
-                  },
-                ),
-                TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(hintText: 'Senha'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Campo obrigatório';
-                      }
-
-                      return null;
-                    }),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: ElevatedButton(
-                    onPressed: () => _authStore.isAuthenticating ? null : _handleSubmit(context),
+                  }),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Observer(builder: (_) {
+                  return ElevatedButton(
+                    key: const Key('loginButton'),
+                    onPressed: () => _authStore.isAuthenticating
+                        ? null
+                        : _handleSubmit(context),
                     child: _authStore.isAuthenticating
                         ? const SizedBox(
                             width: 24,
@@ -74,13 +82,14 @@ class _LoginFormState extends State<LoginForm> {
                             ),
                           )
                         : const Text('Entrar'),
-                  ),
-                ),
-              ],
-            ),
+                  );
+                }),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 
   _handleSubmit(BuildContext context) {
