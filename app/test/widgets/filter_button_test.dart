983626a3e4a2e2159bb7e383712fd2f36125d458
Gustavo Ramos <gustavoloureiroramos@gmail.com>
2023-11-12 17:51:31 +0100

    minor_fix

--- app/test/widgets/filter_button_test.dart
+++ app/test/widgets/filter_button_test.dart
@@ -13,20 +13,22 @@ void main() {
 
   group('FilterButton widget', () {
     testWidgets('FilterButton\'s color changes when market(s) are selected', (tester) async {
-      final filtersStore = Modular.get<FiltersStore>();
-      await tester.pumpWidget(
-        const MaterialApp(
-            home: Scaffold(
-          body: FilterButton(),
-        )),
-      );
+      (tester) async {
+        final filtersStore = Modular.get<FiltersStore>();
+        await tester.pumpWidget(
+          const MaterialApp(
+              home: Scaffold(
+            body: FilterButton(),
+          )),
+        );
 
-      expect(find.byType(ElevatedButton), findsOneWidget);
-      // The following "resolve" method is used to get the color value based on the current state of the widget
-      expect(tester.widget<ElevatedButton>(find.byType(ElevatedButton)).style!.backgroundColor!.resolve({}), Colors.grey);
-      filtersStore.changeSelectedMarkets(Market(id: 0, name: 'Walmart', website: 'walmart.com'), true);
-      await tester.pump();
-      expect(tester.widget<ElevatedButton>(find.byType(ElevatedButton)).style!.backgroundColor!.resolve({}), Colors.blue);
+        expect(find.byType(ElevatedButton), findsOneWidget);
+        // The following "resolve" method is used to get the color value based on the current state of the widget
+        expect(tester.widget<ElevatedButton>(find.byType(ElevatedButton)).style!.backgroundColor!.resolve({}), Colors.grey);
+        filtersStore.changeSelectedMarkets(Market(id: 0, name: 'Walmart', website: 'walmart.com'), true);
+        await tester.pump();
+        expect(tester.widget<ElevatedButton>(find.byType(ElevatedButton)).style!.backgroundColor!.resolve({}), Colors.blue);
+      };
     });
 
     testWidgets('A modal is open when the user clicks on FilterButton', (tester) async {
